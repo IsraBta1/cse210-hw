5db1eb7452c2ec350cd474b1d123111c3238fdb7
israbta18@gmail.com <israbta18@gmail.com>
2026-07-07 01:23:27 -0400

    new element exercise 4 and 5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            string userResponse = Console.ReadLine();
+            int userNumber = int.Parse(userResponse);
+
+            if (userNumber == 0)
+            {
+                break;
+            }
+
+            numbers.Add(userNumber);
+        }
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+            return;
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        double average = (double)sum / numbers.Count;
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
+
+        Console.ReadLine();
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userAge = PromptUserAge();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, userAge, squaredNumber);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program function!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int PromptUserAge()
+    {
+        Console.Write("Please enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        return age;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int age, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square} and your age is {age}.");
     }
 }
