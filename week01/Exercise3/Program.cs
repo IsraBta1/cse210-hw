70c12273bd5b5ef8668ea0924a970346e6c4bc51
israbta18@gmail.com <israbta18@gmail.com>
2026-07-07 00:43:31 -0400

    ramdom

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,39 @@
 using System;
-
+using System.Security.Cryptography;
+// while
+// do-while
+// for
+// foreach
 class Program
+
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    { Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 10);
+        int guess;
+        int attempts = 0;
+
+        Console.WriteLine("Guess the number between 1 and 10!");
+
+        do
+        {
+            Console.Write("Enter your guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            attempts++;
+
+            if (guess < number)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations! You've guessed the number {number} in {attempts} attempts.");
+            }
+        } while (guess != number);
+
     }
 }
