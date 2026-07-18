17629a0cbd078180a12be64bc9d8646d59623ec8
israbta18@gmail.com <israbta18@gmail.com>
2026-07-18 02:23:47 -0400

    three new elemente cs on the folder week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "BYU-Idaho";
+        job1._jobTitle = "Student - Software Development";
+        job1._startYear = 2025;
+        job1._endYear = 2028;
+
+        Resume myResume = new Resume();
+        myResume._name = "Israel Betancourt";
+        myResume._jobs.Add(job1);
+
+        myResume.Display();
     }
 }
